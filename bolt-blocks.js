062d338a75fffc1666f0d6daea8e44ef4b4649db
bolt1117 <59941020+bolt1117@users.noreply.github.com>
2020-03-02 19:02:21 -0600

    Add files via upload

---
+++ bolt-blocks.js
@@ -0,0 +1,24 @@
+class BoltBlocks {
+    getInfo() {
+        return {
+            "id": "BoltBlocks",
+            "name": "BoltBlocks",
+            "blocks": [{
+                "opcode": "reporter",
+                "blockType": "reporter",
+                "text": "report [text]",
+                "arguments": {
+                    "text": {
+                        "type": "string",
+                        "defaultValue": "Hello world!"
+                    }
+                }
+            }],
+            "menus": {},
+        }
+    };
+    reporter({text}) {
+        return text
+    };
+}
+Scratch.extensions.register(new BoltBlocks());
